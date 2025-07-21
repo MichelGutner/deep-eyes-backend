4d83b1feac8c75033da5e54812876b6c567a5a7b
Michel Gutner <michel.gutner@sanar.com>
2025-07-20 23:59:07 -0300

    feat: add elasticsearch

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { MetricsModule } from './metrics/metrics.module';
 import { PrismaModule } from './modules';
 import { LogsModule, LogsController } from './modules/logs';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   controllers: [LogsController],
-  imports: [ConfigModule.forRoot(), PrismaModule, LogsModule, MetricsModule],
+  imports: [PrismaModule, LogsModule, MetricsModule],
   providers: [],
 })
 export class AppModule {}
